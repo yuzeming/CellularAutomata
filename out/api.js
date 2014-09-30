071d1d0a332975fcf1a441c3ee984a7ae4a0f4ca
Zeming YU <yuzeming@gmail.com>
2014-09-30 22:02:14 +0800

    添加文档。

---
+++ out/api.js
@@ -0,0 +1,26 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Cell",
+        "CellDiv",
+        "Game",
+        "TestClass"
+    ],
+    "modules": [
+        "CellularAutomate",
+        "CellularAutomateTest"
+    ],
+    "allModules": [
+        {
+            "displayName": "CellularAutomate",
+            "name": "CellularAutomate",
+            "description": "细胞自动机"
+        },
+        {
+            "displayName": "CellularAutomateTest",
+            "name": "CellularAutomateTest",
+            "description": "细胞自动机测试"
+        }
+    ]
+} };
+});
