98651033da8cc35694431f2115d1ab723ded108d
lishuwnc <wnctsinghua@163.com>
2014-09-30 09:06:40 +0800

    添加注释,修复bug

--- test.js
+++ test.js
@@ -1,11 +1,21 @@
-function map_toObject(Map) {
-    var h = Map.length;
-    var w = Map[0].length;
+/**
+ * 细胞自动机测试
+ *
+ * @module CellularAutomateTest
+ */
+
+/** 过滤Game.Map中无关信息
+ *
+ *  返回仅存储细胞状态值数组
+ */
+function map_toObject() {
+    var h = Game.h;
+    var w = Game.w;
     var convertResult = [];
     for (var i = 0; i < h; i++) {
         var line = [];
         for (var j = 0; j < w; j++) {
-            if (Map[i][j]._state) {
+            if (Game.Map[i][j]._state) {
                 line.push(1);
             }
             else {
@@ -17,6 +27,13 @@ function map_toObject(Map) {
     return convertResult;
 }
 
+/** 测试数据
+ *  testdata[i].h 数据行数
+ *  testdata[i].w 数据列数
+ *  testdata[i].map 棋盘描述
+ *  resultdata[i].map 下一代棋盘描述
+ *  testNum 测试总数
+ */
 testdata = [];
 resultdata = [];
 testNum = 1;
@@ -34,23 +51,29 @@ resultdata.push([[1, 0, 0, 1],
                  [0, 0, 0, 0],
                  [1, 0, 0, 1]]);
 
+/** 根据testdata指定当前状态
+ *
+ *  testdata 测试数据对象
+ */
 function setMap(testdata) {
-    CleanAll();
+    Game.CleanAll();
     var h = testdata.h;
     var w = testdata.w;
+    Game.h = h;
+    Game.w = w;
     for (var i = 0; i < h; i++) {
         var line = [];
         for (var j = 0; j < w; ++j) {
             line.push(new Cell(i, j, (testdata.map[i][j])));
         }
-        Map.push(line);
+        Game.Map.push(line);
     }
 }
 
 test('cell_logic_test', function () {
     for (var i = 0; i < testNum; ++i) {
         setMap(testdata[i]);
-        deepEqual(map_toObject(Move()), resultdata[i], 'Move() test');
+        deepEqual(map_toObject(Game.Move()), resultdata[i], 'Move() test');
 
     }
 })
